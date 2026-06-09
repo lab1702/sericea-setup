6c74fd37d012b6f7e380a4331aebbf844ce9f5ce
Lars Bernhardsson <lab1702@gmail.com>
2026-06-08 21:46:49 -0400

    Create setup script for GNOME and Sway configuration
    Setup script to configure GNOME and Sway settings.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+mkdir -p ~/.config/sway/config.d
+echo 'output * bg #000000 solid_color' > ~/.config/sway/config.d/99-background.conf
+
+mkdir -p ~/.config/foot
+echo 'font=monospace:size=14' > ~/.config/foot/foot.ini
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
