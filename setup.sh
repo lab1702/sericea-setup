a3cb59f698e68add956f614823e4a0102c695600
Lars Bernhardsson <lab1702@gmail.com>
2026-07-23 21:13:42 -0400

    Update setup script for GTK theme configuration
    Removed GTK settings and added environment configuration for dark theme.

--- setup.sh
+++ setup.sh
@@ -2,13 +2,8 @@
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-mkdir -p ~/.config/gtk-3.0
-
-cat > ~/.config/gtk-3.0/settings.ini <<EOF
-[Settings]
-gtk-theme-name=Adwaita-dark
-gtk-application-prefer-dark-theme=1
-EOF
+mkdir -p ~/.config/environment.d
+echo 'GTK_THEME=Adwaita:dark' > ~/.config/environment.d/gtk.conf
 
 mkdir -p ~/.config/sway/config.d
 echo 'output * bg #000000 solid_color' > ~/.config/sway/config.d/99-background.conf
