34ed34d5a575c9dad2ce4303a08fbb617de9e5d9
Lars Bernhardsson <lab1702@gmail.com>
2026-07-23 20:59:33 -0400

    Set GTK theme to Adwaita-dark in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
 mkdir -p ~/.config/sway/config.d
 echo 'output * bg #000000 solid_color' > ~/.config/sway/config.d/99-background.conf
