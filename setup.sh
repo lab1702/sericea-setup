1c4dff2683278ad5d6dd03d802bcbf78942922b4
Lars Bernhardsson <lab1702@gmail.com>
2026-07-23 21:03:09 -0400

    Update setup.sh for GTK and Sway configuration
    Create GTK settings for dark theme and configure Sway background.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+
+mkdir -p ~/.config/gtk-3.0
+
+cat > ~/.config/gtk-3.0/settings.ini <<EOF
+[Settings]
+gtk-theme-name=Adwaita-dark
+gtk-application-prefer-dark-theme=1
+EOF
+
 
 mkdir -p ~/.config/sway/config.d
 echo 'output * bg #000000 solid_color' > ~/.config/sway/config.d/99-background.conf
